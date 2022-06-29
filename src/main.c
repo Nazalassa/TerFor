3a0c8a49bdd4fc11a382ca8bc25721060d2fc4e2
Nazalassa <104462053+Nazalassa@users.noreply.github.com>
2022-06-29 13:45:32 +0200

    alpha 0.0.1

--- src/main.c
+++ src/main.c
@@ -16,64 +16,109 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 **/
 
+// COMPILATION: gcc -o tf-a tf-a.c -lncursesw
+// --------------------------------------- HEADERS ------------------------------------------
+
+#include <ncurses.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 
-struct content{
-	char *author;
-	int date;
-	char *text;
+// --------------------------------------- STRUCTS ------------------------------------------
+
+struct thread{
+  char *content;
+  struct thread *next;
 };
+typedef struct thread THREAD;
 
-struct post{
-	struct content *content;
-	struct post *previous;
-	struct post *next;
+struct metathread{
+  THREAD *thread;
+  int len;
 };
+typedef struct metathread MTHREAD;
 
-typedef struct content content;
-typedef struct post post;
+// --------------------------------- GLOBALS AND CONSTANTS ----------------------------------
 
-char *renderPost(content *post, char type){
-	char *r, *i, *j;
-	if(type){
-		r = malloc(sizeof(char));
-		*r = 'A';
-	}
-	else{
-		int sz = 3*sizeof(char) + sizeof(post->author) + sizeof(post->text);
-		r = malloc(sz);
-		*r = '[';
-		for(i = r+1, j = post->author; i <= r+1+sizeof(post->author); i++, j++){
-			*i = *j;
-		}
-		*i = ']';
-		*(i+1)  = ' ';
-		i += 2;
-		for(j = post->text; i < r + sz; i++, j++){
-			*i = *j;
-		}
-	}
-	return(r);
+
+
+// ---------------------------------- FUNCTION PROTOTYPES -----------------------------------
+
+void displayPosts(MTHREAD*, WINDOW*);
+
+void addPost(MTHREAD*, char*, char*);
+
+// ---------------------------------- FUNCTION DEFINITIONS ----------------------------------
+
+void displayPosts(MTHREAD *postList, WINDOW *postWin){
+  THREAD *reading;
+  reading = postList->thread;
+  short int winX, winY, lines, linesDisplayed;
+  getmaxyx(postWin, winY, winX);
+  short int currentY = winY;
+  wclear(postWin);
+  while(reading != NULL && linesDisplayed < winY){
+    lines = strlen(reading->content) / winX + 1;
+    linesDisplayed += lines, currentY -= lines;
+    mvwprintw(postWin, currentY, 0, reading->content);
+    reading = reading->next;
+  }
+  wrefresh(postWin);
 }
 
-int main(int argc, char *argv[]){
-	FILE *launch = fopen("Data/launch.ascii", "r");
-	int c;
-	if(launch){
-		while((c = getc(launch)) != EOF)
-			printf("%c", c);
-		fclose(launch);
-	}
-	content mypost;
-	mypost.author = "Nazalassa", mypost.text = "HELLO!";
-	char *toPrint = renderPost(&mypost, 0);
-	printf("%s\n", toPrint);
-	/**printf("TerFor  Copyright (C) 2022  Nazalassa");
-	printf("This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.");
-	printf("This is free software, and you are welcome to redistribute it");
-	printf("under certain conditions; type `show c' for details.");**/
-	
-	return(0);
+void addPost(MTHREAD *mainThread, char *post, char *author){
+  THREAD *newPost;
+  newPost = malloc(sizeof(THREAD));
+  newPost->content = (char*)malloc(strlen(author) + strlen(post) + 4);
+  strcpy(newPost->content, "[");
+  strcat(newPost->content, author);
+  strcat(newPost->content, "] ");
+  strcat(newPost->content, post);
+  newPost->next = mainThread->thread;
+  mainThread->thread = newPost;
+  mainThread->len++;
+}
+
+char *usrs[10] = {"Jebediah", "Valentina", "Jebediah", "KSC", "Jebediah", "KSC", "Valentina", "Jebediah", "KSC", "Valentina"};
+char *psts[10] = {"So we're getting near Duna.", "Yup! I can't wait until the landing! When is the maneuver?", "Uh, I don't really know. KSC, when is the maneuver?", "Well, in a few hours.", "Only? I thought we'd have more time.", "Well, we can't really delay a Periapsis Burn, you know.", "Anyway, the guys down on the surface surely wait us. That'll be cruel to make them wait!", "Sure! How long will it take until we're on the ground of Duna?", "Maybe seven hours, not more.", "Yay! Thanks, KSC. I'm waiting for the burn."};
+
+
+// --------------------------------------- MAIN ---------------------------------------------
+
+int main(int argc, char **argv){
+  initscr();
+  raw();
+  noecho();
+  keypad(stdscr, TRUE);
+  // END OF CURSES INITIALIZATION
+  // ...
+  // WINDOW INITIALIZATION
+  short int termX, termY;
+  getmaxyx(stdscr, termY, termX);
+  char numWrite = termY < 25 ? 4 : termY / 6;
+  char postWriteY = termY - numWrite;
+  char numPosts = termY - numWrite - 1;
+  WINDOW *input, *postArea, *separator, *startwin;
+  input = newwin(postWriteY, termX, numPosts + 1, 0), postArea = newwin(numPosts, termX, 0, 0), separator = newwin(1, termX, numPosts, 0);
+  wmove(separator, 0, 0);
+  for(int i = 0; i < termX; i++) waddch(separator, '=');
+  // END OF WINDOW INITIALIZATION
+  // ...
+  wrefresh(separator);
+  MTHREAD *mainThread;
+  mainThread = malloc(sizeof(MTHREAD));
+  mainThread->len = 0, mainThread->thread = NULL;
+  displayPosts(mainThread, postArea);
+  for(int i = 0; i < 10; i++){
+    wgetch(input);
+    addPost(mainThread, psts[i], usrs[i]);
+    displayPosts(mainThread, postArea);
+  }
+  // ...
+  // SHUTTING DOWN
+  delwin(postArea);
+  delwin(separator);
+  delwin(input);
+  endwin();
+  return(0);
 }
